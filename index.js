f3eab2d04aafa4b450974b281345f3cca94cede2
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-12-10 22:42:23 -0500

    WIP

---
+++ index.js
@@ -0,0 +1,34 @@
+const { SpCoinAccessModules } = require("../spcoin-access-modules/index");
+const { 
+  WethMethods,
+  ETHEREUM,
+  POLYGON,
+  HARDHAT,
+  BURN_ADDRESS 
+}  = require("../spcoin-weth-module/index")
+// }  = require("@sponsorcoin/weth-module/wethMethods")
+
+// const {
+//   WethMethods,
+//   ETHEREUM,
+//   POLYGON,
+//   HARDHAT,
+//   BURN_ADDRESS
+// } = require("./spcoin-weth-module/wethMethods");
+  
+class AccessModules {
+    constructor( _signer, _spCoinABI, _spCoinAddress, _dump = true) {
+    this.pCoinClassModules = new SpCoinAccessModules(_spCoinAddress, _spCoinABI, _signer);
+    this.wethMethods = new WethMethods( _weth9Address, _weth9ABI, _signer, _dump )
+  }
+}
+
+module.exports =  {
+  AccessModules,
+  SpCoinAccessModules,
+  WethMethods,
+  BURN_ADDRESS,
+  ETHEREUM,
+  POLYGON,
+  HARDHAT
+}
